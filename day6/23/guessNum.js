1295a33c70045b77a04153d74dcf8af51ca30cb6
Gourav soni <gouravsoni8476@gmail.com>
2024-01-16 20:11:10 +0530

    completed day6th 23 guessGame

--- day6/23/guessNum.js
+++ day6/23/guessNum.js
@@ -1,22 +1,27 @@
-// function guessNum() {
-//   let max = 50;
-//   let randNum = Math.floor(Math.random() * max + 1);
+function guessNum() {
+  const range = prompt("input the number for range");
+  let life = 5;
+  const randomNum = Math.floor(Math.random() * range + 1);
 
-//   let life = 5;
-//   console.log(randNum);
-//   while (life >= 0) {
-//     life--;
-//   }
-// }
-// guessNum();
-
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Enter your name: ', (name) => {
-  console.log(`Hello, ${name}!`);
-  rl.close();
-});
+  let guessedNo = prompt("guess the number");
+  while (life > 0) {
+    console.log(range, life, randomNum, guessedNo);
+    if (randomNum == guessedNo) {
+      alert("You win, number was", guessedNo);
+      break;
+    } else if (randomNum > guessedNo) {
+      alert("Hint: think big");
+      life--;
+      guessedNo = prompt("guess the number");
+    } else if (randomNum < guessedNo) {
+      alert("Hint: think small");
+      life--;
+      guessedNo = prompt("guess the number");
+    } else {
+      life--;
+      guessedNo = prompt("You are wrong, try again");
+    }
+  }
+  if (life == 0) alert("you loose");
+}
+guessNum();
