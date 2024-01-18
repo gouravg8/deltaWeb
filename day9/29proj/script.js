f069c6833823084c9913991a637fe2a9c592baf3
Gourav soni <gouravsoni8476@gmail.com>
2024-01-18 22:01:37 +0530

    completed todo app with deleting fnlitys

--- day9/29proj/script.js
+++ day9/29proj/script.js
@@ -6,13 +6,30 @@ function handleSubmit() {
   console.log(inp.value);
   let li = document.createElement("li");
   li.textContent = inp.value;
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "delete";
+  li.appendChild(deleteBtn);
+
   if (inp.value) taskbox.appendChild(li);
-  else console.log('value to daal');
+  else console.warn("value to daal");
   inp.value = "";
   inp.focus = true;
+
+  //   deleteBtn.onclick = (e) => {
+  //     e.target.parentElement.remove();
+  //   };
 }
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
 btn.addEventListener("click", handleSubmit);
+
+let ul = document.querySelector("ul");
+ul.addEventListener("click", (e) => {
+  //   e.target.parentElement.remove();
+  if (e.target.nodeName == "BUTTON") {
+    e.target.parentElement.remove();
+  }
+});
