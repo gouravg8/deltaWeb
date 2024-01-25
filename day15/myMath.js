da936c4c1f23ca242a671c18278eb174a2ac1172
Gourav soni <gouravsoni8476@gmail.com>
2024-01-25 21:43:04 +0530

    doing day15, 35unka, learnt process,import, export

---
+++ day15/myMath.js
@@ -0,0 +1,6 @@
+export let sum = (...nums) => nums.reduce((a, b) => a + b);
+let multi = (a, b) => a * b;
+const g = 9.8,
+  pi = 3.14;
+
+export default "hello";
---
+++ day15/script.js
@@ -0,0 +1,14 @@
+// let n = 5;
+// for(let i = 0; i < n; i++){
+// 	console.log('hello biro', i);
+// }
+// console.log('bye biro');
+//
+
+// let arg = process.argv;
+// for(let i = 2; i < arg.length; i++){
+// 	console.log('hello', arg[i]);
+// }
+
+import { sum } from "./myMath.js";
+console.log(sum(3,4,5));
