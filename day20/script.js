795018338c4a96e58d69d31bdbec97a5cead160c
Gourav soni <gouravsoni8476@gmail.com>
2024-01-31 22:12:42 +0530

    implimented edit and delete fn

--- day20/script.js
+++ day20/script.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
+import methodOverride from "method-override";
 const app = express();
 const port = 8080;
 
@@ -10,6 +12,9 @@ const __dirname = dirname(__filename);
 // accept json data from url
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// method override
+app.use(methodOverride("_method"));
 
 // view engine
 app.set("view engine", "ejs");
@@ -35,11 +40,45 @@ let posts = [
   {
     id: "1c",
     username: "anu",
-    caption: "badshah he badshah",
+    caption: "badshah he badshah ",
     image: "anu.jpg",
   },
 ];
 
 app.get("/", (req, res) => {
   res.render("home", { posts });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/new", (req, res) => {
+  let id = uuidv4();
+  const { username, caption, image } = req.body;
+  posts.push({ id, username, caption, image });
+  // console.log(id, username, caption, image);
+  // console.log(posts);
+  // console.log(req);
+  res.redirect("/");
+});
+
+app.get("/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((p) => p.id === id);
+  res.render("edit", { post });
+});
+
+app.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((p) => p.id === id);
+  post.caption = req.body.caption;
+  res.redirect("/");
+});
+
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  posts = posts.filter((p) => p.id !== id);
+  console.log(posts);
+  res.redirect("/");
 });
