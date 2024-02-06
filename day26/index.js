4c39803821df77760530f4d2b4f574b519e32c23
Gourav soni <gouravsoni8476@gmail.com>
2024-02-06 20:34:50 +0530

    basic setup of the project

---
+++ day26/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import methodOverride from "method-override";
+import Chat from "./models/chat.js";
+
+const app = express();
+const port = 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// handling post req data, and sending post data into json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// override method
+app.use(methodOverride("_method"));
+// set path of views(ejs)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+// set public folder path
+app.use(express.static(path.join(__dirname, "public")));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+main()
+  .then((res) => console.log("mongo se connect ho gya"))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.redirect("/chats");
+});
+
+
+app.listen(port, () => console.log(`port is on: ${port}`));
