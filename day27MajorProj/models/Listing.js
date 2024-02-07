12daac5160b718cfb92eccb17ff5e35630a85228
Gourav soni <gouravsoni8476@gmail.com>
2024-02-07 23:03:06 +0530

    created Listing model

---
+++ day27MajorProj/models/Listing.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const defaultImageLink =
+  "https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+const Schema = mongoose.Schema;
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default: defaultImageLink,
+    set: (v) => (v === "" ? defaultImageLink : v),
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+export default Listing;
