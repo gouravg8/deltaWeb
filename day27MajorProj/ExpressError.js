feb86e005e4a3a60fef687d26f014199e56f1bcb
Gourav soni <gouravsoni8476@gmail.com>
2024-02-13 13:40:25 +0530

    moved to utils

--- day27MajorProj/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error {
-  constructor(status = 500, message = "Galti ho gyi") {
-    super();
-    this.status = status;
-    this.message = message;
-  }
-}
-
-export default ExpressError;
