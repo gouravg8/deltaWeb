d6d37a454778ef0d7db283782143c30473bb29ef
Gourav soni <gouravsoni8476@gmail.com>
2024-02-15 23:16:49 +0530

    created schema for reviews

---
+++ day27MajorProj/models/Review.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  comment: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Review = new mongoose.model("Review", reviewSchema);
+
+export default Review;
