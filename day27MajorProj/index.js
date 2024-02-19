07fc0b2800c8ebcfb7a7146fbc607039490c0c98
Gourav soni <gouravsoni8476@gmail.com>
2024-02-19 22:49:50 +0530

    added error to res.local

--- day27MajorProj/index.js
+++ day27MajorProj/index.js
@@ -51,6 +51,7 @@ app.get("/", (req, res) => res.redirect("/listings"));
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
 });
 
