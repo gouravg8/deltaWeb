38feb2ecd37ff03501edbf522dcd073e2f29ff23
Gourav soni <gouravsoni8476@gmail.com>
2024-02-21 22:39:22 +0530

    saving redirected url before login/signup in session and locals

--- day27MajorProj/middleware.js
+++ day27MajorProj/middleware.js
@@ -1,9 +1,18 @@
 const isLoggedin = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    // redirect after login or sign up to desired path
+    req.session.redirectUrl = req.originalUrl;
     req.flash("error", "You are not logged in!");
     return res.redirect("/login");
   }
   next();
 };
 
-export {isLoggedin}
+const saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
+
+export { isLoggedin, saveRedirectUrl };
