666bb124cd167ba2983a92c2722ad803d7eee573
Gourav soni <gouravsoni8476@gmail.com>
2024-02-23 22:42:48 +0530

    added isOwner, validateSchema, validateReviewSchema mw

--- day27MajorProj/middleware.js
+++ day27MajorProj/middleware.js
@@ -1,3 +1,6 @@
+import Listing from "./models/Listing.js";
+import ExpressError from "./utils/ExpressError.js";
+import listingSchema, { reviewSchema } from "./schema.js";
 const isLoggedin = (req, res, next) => {
   if (!req.isAuthenticated()) {
     // redirect after login or sign up to desired path
@@ -15,4 +18,39 @@ const saveRedirectUrl = (req, res, next) => {
   next();
 };
 
-export { isLoggedin, saveRedirectUrl };
+const isOwner = async (req, res, next) => {
+  const { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!res.locals.currUser._id.equals(listing.owner._id)) {
+    req.flash("error", "You are not the owner of this content");
+    res.redirect(`/listings/${id}`);
+  } else next();
+};
+
+// validate listing Schema
+const validateSchema = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  // console.log(req.body);
+  if (error) {
+    console.log("listingValidateErrorHai", error);
+    throw new ExpressError(400, error.message);
+  } else next();
+};
+
+// validate review schema
+const validateReviewSchema = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  // console.log(req.body);
+  if (error) {
+    console.log("reviewValidateErrorHai", error);
+    throw new ExpressError(400, error.message);
+  } else next();
+};
+
+export {
+  isLoggedin,
+  saveRedirectUrl,
+  isOwner,
+  validateSchema,
+  validateReviewSchema,
+};
