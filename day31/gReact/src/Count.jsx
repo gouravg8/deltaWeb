4c249eeb319f36011d0530bf7bd809f4567ba9ab
Gourav soni <gouravsoni8476@gmail.com>
2024-03-03 20:49:27 +0530

    learning events, created ig like btn

---
+++ day31/gReact/src/Count.jsx
@@ -0,0 +1,37 @@
+// import { useState } from "react";
+// import ReRender from "./ReRender";
+
+function Count() {
+  //   let [like, setLike] = useState("regular");
+  // const [isLike, setIsLike] = useState(false);
+  //   way one
+  //   function handleLike() {
+  //     like == "regular" ? setLike("solid") : setLike("regular");
+  //   }
+
+  //   way2
+  // function handleLike() {
+  //   setIsLike(!isLike);
+  // }
+  return (
+    <>
+      {/* <p
+        className="my-12 text-3xl text-red-500 cursor-pointer"
+        onClick={handleLike}
+      > */}
+      {/* <i
+          className={`fa-${like} fa-heart cursor-pointer text-red-600`}
+          onClick={handleLike}
+        ></i> */}
+      {/* {isLike ? (
+          <i className="fa-solid fa-heart"></i>
+        ) : (
+          <i className="fa-regular fa-heart"></i>
+        )}
+      </p> */}
+      {/* <ReRender /> */}
+    </>
+  );
+}
+
+export default Count;
